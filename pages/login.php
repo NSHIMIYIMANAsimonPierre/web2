5e2fc1469bda852ffdfe02459e29ceb34bf6823a
unknown <nshicesimonpierre007@gmail.com>
2022-08-02 23:32:15 +0200

    initial commit

---
+++ pages/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>login form</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+   <div class="login-box">
+    <h1>injirira hano</h1>
+    <form method="post">
+           
+            <label>amazina</label><input type="text" required placeholder="amazina"> <br> 
+            <label>email    </label><input type="text" required placeholder="email"> <br> 
+           
+            <label>ijambo banga </label><input type="password" required placeholder="ijambo banga"><br> 
+           <div class="pass">wibagiwe ijambo banga? <a href="#">wuhindure</a></div>
+           <input type="submit" id="submitbtn" name="submit"  value="injira"  required>
+           <div class="signup_link">
+         <p> Nta konti ufitemo?</p><a href="register.html">iyandikishe</a>
+           </div>
+
+    </form>
+   </div> 
+</body>
+</html>]
---
+++ pages/register.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>accout</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <div class="form_container">
+        <div class="overlay">
+            <!------this will have no contents-->
+        </div>
+        <div class="titlediv">
+           <h1 class="title">iyandikishe</h1>
+           <span class="subtitle">muhisemo neza</span>
+        </div>
+        <!------form section-->
+        <form action="" method="">
+            <div class="rowgrid">
+                <div class="row">
+                <label for="username">amazina</label>
+                <input type="text" id="username" name="amzina" placeholder="shyiramo amazina" required>
+    
+                </div>
+                <div class="row">
+                    <label for="telnbr">telephone</label>
+                    <input type="number" id="nimero" name="tel" placeholder="shyiramo nimero" required>
+                    
+                    </div>
+                    <div class="row">
+                        <label for="emailname">email</label>
+                        <input type="text" id="email" name="email" placeholder="shyiramo email" required>
+                        
+                        </div>
+                        <div class="row">
+                            <label for="password">ijambo banga</label>
+                            <input type="password" id="passname" name="pass" placeholder="shyiramo umubare wibanga" required>
+                            
+                            </div>
+                            <div class="row">
+                                
+                                <input type="submit" id="submitbtn" name="submit" value="emeza"  required>
+                                <span class="registerlink"><p>ufite konti hano? </p><a href="login.html">injira hano</a></span>
+                                </div>
+            </div>
+        </form>
+       </div> 
+</body>
+</html>
